c70933b9c7e175f7b9fdf45e4a1818b572f83ccb
Khampasert9328 <109733313+Khampasert9328@users.noreply.github.com>
2022-08-09 09:54:18 +0700

    Update bussiness_logic.dart

--- lib/bussiness logic/bussiness_logic.dart
+++ lib/bussiness logic/bussiness_logic.dart
@@ -6,9 +6,9 @@ class AuthenticationProvider extends ChangeNotifier{
 
   Future<void> login(String email, String name, String password)async{
     ConnectValidateModels? connectValidateModels = await connectvalidateuse(email);
-
+  // ວິທີການເຊັກເງື່ອນໄຂກວດ User
     if (connectValidateModels != null) {
-      
+      //ຖ້າວ່າ connectValidateModels ບໍ່ເທົ່າກັບຄ່າວ່າງ ໃຫ້ມັນ Access ຂໍ Token
     }
 
     
